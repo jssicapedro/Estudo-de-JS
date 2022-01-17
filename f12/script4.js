3ece6ec3d2f176ababb0eeca8de8f4ff5e09bb26
Jéssica A.P.P <jessicapedro165@gmail.com>
2022-01-17 14:34:42 +0000

    é isso

--- f12/script4.js
+++ f12/script4.js
@@ -1,7 +1,11 @@
 function minusculas(){
-
+    let min = document.getElementById("box");
+    
+    min.value = min.value.toLocaleLowerCase();
 }
 
 function maiusculas(){
-    
+    let mai = document.getElementById("box");
+
+    mai.value = mai.value.toLocaleUpperCase();
 }
