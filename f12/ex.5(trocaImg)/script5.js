c4ad130f277de7b5a7b4416c1b21c4c5a586af62
Jéssica Pedro <jessicapedro165@gmail.com>
2022-01-19 22:41:48 +0000

    ex5

--- f12/ex.5(trocaImg)/script5.js
+++ f12/ex.5(trocaImg)/script5.js
@@ -1,3 +1,9 @@
 function alteraImagem(){
-    
+    let img = document.querySelector("img");
+
+    if(img.getAttribute("src", "images/chuva.jpg")){
+        img.setAttribute("src", "images/sol.jpg");
+    } else{
+        img.setAttribute("src", "images/chuva.jpg");
+    }
 }
