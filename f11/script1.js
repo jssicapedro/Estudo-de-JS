3d276380ecd044fcd8d1c77eda71e26f4152f35a
Jéssica Pedro <jessicapedro165@gmail.com>
2022-01-20 22:33:22 +0000

    a

--- f11/script1.js
+++ f11/script1.js
@@ -1,1 +1,17 @@
-//ler 5 numeros
+//ler 5 numeros e apresentar num array
+
+function lerNum(){
+    do {
+        let n = Number(prompt("Indique um valor"));
+
+        if(isNaN(n)){
+            alert("Porfavor indique um numero!");
+        }
+
+    } while (isNaN(n));
+    
+    return n;
+}
+
+/* cp */
+let num = lerNum();
