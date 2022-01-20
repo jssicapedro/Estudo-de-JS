6a621edf29693800d223da4398e75d0fd7d1d336
Jéssica Pedro <67608094+jssicapedro@users.noreply.github.com>
2022-01-20 22:19:17 +0000

    Update scripr2.js

--- f13/scripr2.js
+++ f13/scripr2.js
@@ -1,15 +1,14 @@
-function adicionarItem(){
-        let txt = document.getElementById("item");
-
-        let lista = document.getElementById("lista");
-
+function adicionarItem(){ 
+//buscar texto da caixa
+        let texto = document.getElementById("item");
+//criar li
         let li = document.createElement("li");
-        
-        li.innerHTML = txt.value;
-        
-        lista.appendChild(li);
-
-        txt.value="";
+//chama o texto para dentro do li
+        li.innerHTML = texto.value;
+//colocar texto e li  dentro do ul
+        document.getElementById("lista").appendChild(li);
+//apagar texto da caixa
+        texto.value = "";
 }
 
 function limpar(){
